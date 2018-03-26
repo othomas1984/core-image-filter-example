c329dc754c69fd8dd6821c541d083b74f455926f
Owen Thomas <owen@owenhthomas.com>
2018-03-26 16:13:29 -0700

    Allow saving high quality image to photo library

--- Core Image Filter Example/ViewController.swift
+++ Core Image Filter Example/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Photos
 
 class ViewController: UIViewController {
   var context: CIContext = CIContext(options: nil)
@@ -22,9 +23,10 @@ class ViewController: UIViewController {
   var throttleInterval = 0.07
   var orientation = UIImageOrientation.up
 
-  func sepiaImage(completion: @escaping (CGImage?) -> Void) {
+  func sepiaImage(originalImage: CIImage? = nil, completion: @escaping (CGImage?) -> Void) {
+    let image: CIImage! = originalImage ?? self.thumbnailImage
     guard let sepiafilter = CIFilter(name: "CISepiaTone", withInputParameters: [
-      kCIInputImageKey: thumbnailImage,
+      kCIInputImageKey: image,
       kCIInputIntensityKey: filterIntensity]),
       let outputImage = sepiafilter.outputImage,
       let filterExtent = sepiafilter.outputImage?.extent else { completion(nil); return }
@@ -61,6 +63,13 @@ extension ViewController {
     imagePickerVC.delegate = self
     present(imagePickerVC, animated: true)
   }
+  
+  @IBAction func saveButtonTapped(_ sender: UIButton) {
+    let spinner = blockViewWithSpinner(view: view)
+    trySaveImage {
+      spinner.removeFromSuperview()
+    }
+  }
 }
 
 // MARK: Private
@@ -95,10 +104,10 @@ extension ViewController {
   
   private func createThumbnail() {
     guard let scaleFilter = CIFilter(name: "CILanczosScaleTransform", withInputParameters: [
-        kCIInputImageKey: originalImage,
-        kCIInputScaleKey: thumbnailScale,
-        kCIInputAspectRatioKey: 1.0
-        ]),
+      kCIInputImageKey: originalImage,
+      kCIInputScaleKey: thumbnailScale,
+      kCIInputAspectRatioKey: 1.0
+      ]),
       let scaled = scaleFilter.outputImage
       else {
         thumbnailImage = originalImage
@@ -107,7 +116,7 @@ extension ViewController {
     thumbnailImage = scaled
   }
   
-  var thumbnailScale: CGFloat {
+  private var thumbnailScale: CGFloat {
     let narrowestPartOfOriginalImage = min(originalImage.extent.width, originalImage.extent.height)
     return min(view.bounds.width / narrowestPartOfOriginalImage, 1.0)
   }
@@ -125,6 +134,69 @@ extension ViewController {
   private var imageViewAspectRatio: CGFloat? {
     guard let size = imageView.image?.size else { return nil }
     return min(max(size.width / size.height, 2/3), 3/2)
+  }
+  
+  private func blockViewWithSpinner(view: UIView) -> UIActivityIndicatorView {
+    let spinner = UIActivityIndicatorView(activityIndicatorStyle: UIActivityIndicatorViewStyle.whiteLarge)
+    spinner.startAnimating()
+    spinner.translatesAutoresizingMaskIntoConstraints = false
+    view.addSubview(spinner)
+    spinner.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+    spinner.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    spinner.heightAnchor.constraint(equalTo: view.heightAnchor).isActive = true
+    spinner.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+    spinner.backgroundColor = UIColor(displayP3Red: 204/255, green: 204/255, blue: 204/255, alpha: 0.5)
+    return spinner
+  }
+  
+  private func trySaveImage(_ completion: @escaping () -> Void) {
+    PHPhotoLibrary.requestAuthorization { status in
+      if status == .authorized {
+        self.saveImage {
+          completion()
+        }
+      } else {
+        self.photosAccessError() {
+          completion()
+        }
+      }
+    }
+  }
+  
+  private func saveImage(completion: @escaping () -> Void) {
+    sepiaImage(originalImage: originalImage) { image in
+      guard let image = image else { return }
+      let uiImage = UIImage(cgImage: image, scale: 1, orientation: self.orientation)
+      PHPhotoLibrary.shared().performChanges({
+        PHAssetChangeRequest.creationRequestForAsset(from: uiImage)
+      }, completionHandler: { (complete, error) in
+        DispatchQueue.main.async {
+          if let error = error {
+            self.imageSaveError(error, completion: completion)
+          } else {
+            self.imageSaveSuccess(completion)
+          }
+        }
+      })
+    }
+  }
+  
+  private func imageSaveSuccess(_ completion: @escaping () -> Void) {
+    let ac = UIAlertController(title: "Success!", message: "The modified image has been saved to your photo library", preferredStyle: .alert)
+    ac.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+    present(ac, animated: true, completion: completion)
+  }
+  
+  private func imageSaveError(_ error: Error, completion: @escaping () -> Void) {
+    let ac = UIAlertController(title: "Error Saving Image", message: error.localizedDescription, preferredStyle: .alert)
+    ac.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+    present(ac, animated: true, completion: completion)
+  }
+  
+  private func photosAccessError(_ completion: @escaping () -> Void) {
+    let ac = UIAlertController(title: "Need Photos Access", message: "Please enable access to photo library in System Privacy Settings", preferredStyle: .alert)
+    ac.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+    present(ac, animated: true, completion: completion)
   }
 }
 
